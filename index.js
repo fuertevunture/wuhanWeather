41c1d67eb662c1583f921a0080fcc3d13e2a0c0b
lvkun <lvkun@skieer.com>
2026-03-19 23:34:59 +0800

    feat:搜索栏

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// 数据
+let searchContent = '';
+
+
 //主题样式按钮的切换
 const headerThemeModeDomList = Array.from(document.querySelectorAll(".header-theme-mode"));
 let hiddenHeaderThemeModeDom = document.querySelector(".hidden_header-theme-mode");
@@ -9,4 +13,29 @@ headerThemeModeDomList.forEach(headerThemeMode => {
         hiddenHeaderThemeModeDom = headerThemeMode;
     })
 });
+
+//搜索栏的聚焦
+const localSearchDom = document.querySelector(".local-search");
+const localSearchInputDom = document.querySelector(".local-search_input");
+
+localSearchInputDom.addEventListener("focus", (e) => {
+    localSearchDom.classList.add("focused_local-search");
+})
+
+localSearchInputDom.addEventListener("blur", (e) => {
+    localSearchDom.classList.remove("focused_local-search");
+})
+
+localSearchInputDom.addEventListener("change", (e) => {
+    searchContent = e.target.value;
+})
+
+localSearchInputDom.addEventListener("input", (e) => {}
+)
+
+const localSearchConfirmDom = document.querySelector(".local-search-confirm");
+
+localSearchConfirmDom.addEventListener("click", (e) => {
+
+})
 
